744ada388166eda83acc3ae1d6fdf3e3b5a1df82
brihjit <brigitte.g.acosta>
2015-02-02 22:29:34 +0000

    numbers game

---
+++ numbersgame.rb
@@ -0,0 +1,20 @@
+puts "Hi, pick a number!"
+number = gets
+puts "Great choice! I like the number " + number + "!"
+
+puts "Okay, pick another number!"
+number = gets
+puts " Awesome, love the number " + number + "!"
+
+puts "Let us try some math"
+puts "Pick a big number"
+a = gets.chomp.to_i
+
+puts "Pick a second, smaller number"
+b= gets.chomp.to_i
+
+puts "Okay! I'm going to divide them!' "
+puts a/b
+
+puts "Thats your answer!"
+
